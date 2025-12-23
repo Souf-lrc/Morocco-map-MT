46e3b22c23a5a3f2d7776557e68d7fd9726b02f4
Souf-lrc <64473069+Souf-lrc@users.noreply.github.com>
2025-12-23 22:19:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def create_map():
 st.title("⚡ Réseau Électrique Maroc")
 
 try:
-    z1, z2, z3, z4p1, z4p2, regions, communes, postes_df = load_data()
+    # z1, z2, z3, z4p1, z4p2, regions, communes, postes_df = load_data()
     m = create_map()
     add_layers(m, regions, communes, postes_df)
     
